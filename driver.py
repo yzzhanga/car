3fb98adc29e1b03ab97dba61b5813987a20bbf02
zhangyingzheng <louis.zhangyz@gmail.com>
2022-03-20 11:04:18 +0800

    代码整理

--- driver.py
+++ driver.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 from time import sleep
-import orderSet
+from orderSet import L298nOrder
 
 #加载指令集
-order = orderSet.L298nOrder
-#装载GPIO输出初始化
-order.setup()
+order = L298nOrder([10,17],[27,22],[18,23], [24,25])
 #底盘自检程序
 def test():
     print("执行指令")
@@ -39,30 +37,36 @@ test()
 
 
 _inputOrder = 'x'
-while True :
+while True:
     sleep(0.5)
-    _inputOrder = input('请输入底盘指令:w-前进,s-后退,a-向左,d-向右,z-左倒车,c-右倒车,x-刹车,p-销毁终止指令,o-重新安装指令:')
+    _inputOrder = input('请输入底盘指令:w-前进,s-后退,a-向左,d-向右,z-左倒车,c-右倒车,x-刹车,p-销毁终止指令:')
     if (_inputOrder == 'w'):
         order.stop()
         order.allMove()
+        continue
     if (_inputOrder == 's'):
         order.stop()
         order.allBack()
+        continue
     if (_inputOrder == 'a'):
         order.stop()
         order.turnLeftRound(False)
+        continue
     if (_inputOrder == 'd'):
         order.stop()
         order.turnRightRound(False)
+        continue
     if (_inputOrder == 'z'):
         order.stop()
         order.turnLeftRound(True)
+        continue
     if (_inputOrder == 'c'):
         order.stop()
         order.turnRightRound(True)
+        continue
     if (_inputOrder == 'x'):
         order.stop()
+        continue
     if (_inputOrder == 'p'):
         order.destory()
-    if (_inputOrder == 'o'):
-        order.setup()
+        break
--- orderSet.py
+++ orderSet.py
@@ -1,92 +1,96 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-from time import sleep
 import RPi.GPIO as GPIO
 
-# POINT = [5,6,16,26]
-# GPIO接口结合
-LEFT_FRONT_PORT = [10,17] #左前轮 正负输出，这两个输出互斥，不能同时为正，但可以同时为负，不管同时为正或负，都视为刹车；
-LEFT_BACK_PORT = [27,22] #左后轮
-RIGHT_FRONT_PORT = [18,23] #右前轮
-RIGHT_BACK_PORT = [24,25] #右后轮
 
-#忽略重复setmode导致的报警，一般是在关闭程序时候没有调用destory方法导致
-GPIO.setwarnings(False)
 
 class L298nOrder:
-    
-    def setup():
-        print("指令集初始化成功")
+    # LEFT_FRONT_PORT  #左前轮 正负输出，这两个输出互斥，不能同时为正，但可以同时为负，不管同时为正或负，都视为刹车；
+    # LEFT_BACK_PORT #左后轮
+    # RIGHT_FRONT_PORT #右前轮
+    # RIGHT_BACK_PORT  #右后轮
+
+    def __init__(self,lp = [10,17],lb= [27,22],rp= [18,23],rb= [24,25]):
+        self.LEFT_FRONT_PORT = lp
+        self.LEFT_BACK_PORT = lb
+        self.RIGHT_FRONT_PORT = rp
+        self.RIGHT_BACK_PORT = rb
+
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(LEFT_FRONT_PORT,GPIO.OUT)
-        GPIO.setup(LEFT_BACK_PORT,GPIO.OUT)
-        GPIO.setup(RIGHT_FRONT_PORT,GPIO.OUT)
-        GPIO.setup(RIGHT_BACK_PORT,GPIO.OUT)
+        GPIO.setwarnings(False)
+        self.setup()
+        print("指令集初始化成功")
+
+    def setup(self):
+        GPIO.setup(self.LEFT_FRONT_PORT,GPIO.OUT)
+        GPIO.setup(self.LEFT_BACK_PORT,GPIO.OUT)
+        GPIO.setup(self.RIGHT_FRONT_PORT,GPIO.OUT)
+        GPIO.setup(self.RIGHT_BACK_PORT,GPIO.OUT)
 
     #关闭GPIO的输出设置（这里有个前提是GPIO全部服务本集合，右可能会误关闭其他GPIO组合程序）
-    def destory():
+    def destory(self):
         print("指令驱动销毁")
         GPIO.cleanup()
 
     #转向程序 左中心线的顺时针和逆时针旋转
-    def turnLeftRound(leftBool):
+    def turnLeftRound(self,leftBool):
         if (leftBool):
-            GPIO.output(LEFT_FRONT_PORT[0],True)
-            GPIO.output(LEFT_FRONT_PORT[1],False)
-            GPIO.output(RIGHT_BACK_PORT[0],False)
-            GPIO.output(RIGHT_BACK_PORT[1],True)
+            GPIO.output(self.LEFT_FRONT_PORT[0],True)
+            GPIO.output(self.LEFT_FRONT_PORT[1],False)
+            GPIO.output(self.RIGHT_BACK_PORT[0],False)
+            GPIO.output(self.RIGHT_BACK_PORT[1],True)
             print ("执行指令:左转向")
         else:
-            GPIO.output(LEFT_FRONT_PORT[0],False)
-            GPIO.output(LEFT_FRONT_PORT[1],True)
-            GPIO.output(RIGHT_BACK_PORT[0],True)
-            GPIO.output(RIGHT_BACK_PORT[1],False)
+            GPIO.output(self.LEFT_FRONT_PORT[0],False)
+            GPIO.output(self.LEFT_FRONT_PORT[1],True)
+            GPIO.output(self.RIGHT_BACK_PORT[0],True)
+            GPIO.output(self.RIGHT_BACK_PORT[1],False)
             print ("执行指令:左转倒车")
     #转向程序 右中心线的顺时针和逆时针旋转
-    def turnRightRound(rightBool):
+    def turnRightRound(self,rightBool):
         if (rightBool):
-            GPIO.output(RIGHT_FRONT_PORT[0],True)
-            GPIO.output(RIGHT_FRONT_PORT[1],False)
-            GPIO.output(LEFT_BACK_PORT[0],False)
-            GPIO.output(LEFT_BACK_PORT[1],True)
+            GPIO.output(self.RIGHT_FRONT_PORT[0],True)
+            GPIO.output(self.RIGHT_FRONT_PORT[1],False)
+            GPIO.output(self.LEFT_BACK_PORT[0],False)
+            GPIO.output(self.LEFT_BACK_PORT[1],True)
             print ("执行指令:右转向")
         else:
-            GPIO.output(RIGHT_FRONT_PORT[0],False)
-            GPIO.output(RIGHT_FRONT_PORT[1],True)
-            GPIO.output(LEFT_BACK_PORT[0],True)
-            GPIO.output(LEFT_BACK_PORT[1],False)
+            GPIO.output(self.RIGHT_FRONT_PORT[0],False)
+            GPIO.output(self.RIGHT_FRONT_PORT[1],True)
+            GPIO.output(self.LEFT_BACK_PORT[0],True)
+            GPIO.output(self.LEFT_BACK_PORT[1],False)
             print ("执行指令:右转倒车")
     #前进挡
-    def allMove():
-        GPIO.output(LEFT_FRONT_PORT[0],True)
-        GPIO.output(LEFT_FRONT_PORT[1],False)
-        GPIO.output(LEFT_BACK_PORT[0],True)
-        GPIO.output(LEFT_BACK_PORT[1],False)
-        GPIO.output(RIGHT_FRONT_PORT[0],True)
-        GPIO.output(RIGHT_FRONT_PORT[1],False)
-        GPIO.output(RIGHT_BACK_PORT[0],True)
-        GPIO.output(RIGHT_BACK_PORT[1],False)
+    def allMove(self):
+        GPIO.output(self.LEFT_FRONT_PORT[0],True)
+        GPIO.output(self.LEFT_FRONT_PORT[1],False)
+        GPIO.output(self.LEFT_BACK_PORT[0],True)
+        GPIO.output(self.LEFT_BACK_PORT[1],False)
+        GPIO.output(self.RIGHT_FRONT_PORT[0],True)
+        GPIO.output(self.RIGHT_FRONT_PORT[1],False)
+        GPIO.output(self.RIGHT_BACK_PORT[0],True)
+        GPIO.output(self.RIGHT_BACK_PORT[1],False)
         print ("执行指令:前进")
     #倒车挡
-    def allBack():
-        GPIO.output(LEFT_FRONT_PORT[0],False)
-        GPIO.output(LEFT_FRONT_PORT[1],True)
-        GPIO.output(LEFT_BACK_PORT[0],False)
-        GPIO.output(LEFT_BACK_PORT[1],True)
-        GPIO.output(RIGHT_FRONT_PORT[0],False)
-        GPIO.output(RIGHT_FRONT_PORT[1],True)
-        GPIO.output(RIGHT_BACK_PORT[0],False)
-        GPIO.output(RIGHT_BACK_PORT[1],True)
+    def allBack(self):
+        GPIO.output(self.LEFT_FRONT_PORT[0],False)
+        GPIO.output(self.LEFT_FRONT_PORT[1],True)
+        GPIO.output(self.LEFT_BACK_PORT[0],False)
+        GPIO.output(self.LEFT_BACK_PORT[1],True)
+        GPIO.output(self.RIGHT_FRONT_PORT[0],False)
+        GPIO.output(self.RIGHT_FRONT_PORT[1],True)
+        GPIO.output(self.RIGHT_BACK_PORT[0],False)
+        GPIO.output(self.RIGHT_BACK_PORT[1],True)
         print ("执行指令:后退")
     #刹车
-    def stop():
-        GPIO.output(LEFT_FRONT_PORT[0],False)
-        GPIO.output(LEFT_FRONT_PORT[1],False)
-        GPIO.output(LEFT_BACK_PORT[0],False)
-        GPIO.output(LEFT_BACK_PORT[1],False)
-        GPIO.output(RIGHT_FRONT_PORT[0],False)
-        GPIO.output(RIGHT_FRONT_PORT[1],False)
-        GPIO.output(RIGHT_BACK_PORT[0],False)
-        GPIO.output(RIGHT_BACK_PORT[1],False)
+    def stop(self):
+        GPIO.output(self.LEFT_FRONT_PORT[0],False)
+        GPIO.output(self.LEFT_FRONT_PORT[1],False)
+        GPIO.output(self.LEFT_BACK_PORT[0],False)
+        GPIO.output(self.LEFT_BACK_PORT[1],False)
+        GPIO.output(self.RIGHT_FRONT_PORT[0],False)
+        GPIO.output(self.RIGHT_FRONT_PORT[1],False)
+        GPIO.output(self.RIGHT_BACK_PORT[0],False)
+        GPIO.output(self.RIGHT_BACK_PORT[1],False)
         print ("执行指令:停止")
 
