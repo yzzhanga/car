1931fe81f04ccd01d42a6b4f0dc1bbf934aca7cd
zhangyingzheng <louis.zhangyz@gmail.com>
2022-04-05 07:50:14 +0800

    小车的程序变更

--- driver.py
+++ driver.py
@@ -1,44 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-from time import sleep
 from orderSet import L298nOrder
 from communication import mqutils
 
 #加载指令集
-order = L298nOrder([10,17],[27,22],[18,23], [24,25])
-#底盘自检程序
-def test():
-    print("执行指令")
-    order.allMove()
-    sleep(2)
-    order.stop()
-
-    order.allBack()
-    sleep(2)
-    order.stop()
-
-    order.turnLeftRound(True)
-    sleep(2)
-    order.stop()
-
-    order.turnLeftRound(False)
-    sleep(2)
-    order.stop()
-
-
-    order.turnRightRound(True)
-    sleep(2)
-    order.stop()
-
-    order.turnRightRound(False)
-    sleep(2)
-    order.stop()
-#自检程序执行
-test()
-
-
-
-redis = mqutils(host='**********', port= 46379, password= '********', db= 0)
+order =L298nOrder([26,19],[13,10],[21,20], [16,12])
+redis =mqutils(host='mq.yzzhanga.xyz', port= 46379, password= '1qaz2wsx', db= 0)
 redis.connection()
 
 redis.receive("car_order_queue",order)
